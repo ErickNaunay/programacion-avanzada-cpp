4f8fe3b1be40c15054828ccde60422ab25edf916
sxlsilent <maldonadobrandon236@gmail.com>
2026-09-06 22:00:44 +0000

    realizado ejercicio 7 pset 1

--- psets/pset-01/ejercicio7_cronometro.cpp
+++ psets/pset-01/ejercicio7_cronometro.cpp
@@ -19,7 +19,8 @@ private:
 
 public:
     Cronometro(double segundosIniciales) {
-        // TODO: asigna segundosIniciales a segundos
+        
+        segundos = segundosIniciales;
     }
 
     double getSegundos() {
