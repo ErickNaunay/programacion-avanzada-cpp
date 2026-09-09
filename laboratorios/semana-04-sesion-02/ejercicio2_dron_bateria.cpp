b9e9c4cf77cca342b15f3e2b069b3913b858a2ee
sxlsilent <maldonadobrandon236@gmail.com>
2026-09-09 12:48:18 +0000

    realizado ejercicio 2 dron bateria

--- laboratorios/semana-04-sesion-02/ejercicio2_dron_bateria.cpp
+++ laboratorios/semana-04-sesion-02/ejercicio2_dron_bateria.cpp
@@ -22,16 +22,29 @@ class Bateria {
             return nivelCargaPorc > 20;
         }
 };
-
-// TODO: Dron no tiene ninguna relacion "es un" con Bateria. Agrega un
-// atributo privado "Bateria bateria;" y escribe:
-// bool despegar(): si la bateria no esta cargada, imprime
-//   "Bateria muy baja, no despega" y devuelve false; si esta cargada,
-//   imprime "Dron despegando con <nivel>% de bateria" y devuelve true.
-// void descargar(int porcentaje): reduce el nivel de carga de la bateria
-//   en ese porcentaje.
 class Dron {
-    // TODO
+    private:
+        Bateria bateria;
+    
+    public:
+        bool despegar(){
+            if (!bateria.estaCargada()) {
+                std::cout << "Bateria muy baja, no despega" << std::endl;
+                    return false;
+
+        }
+
+        std::cout << "Dron despegando con " << bateria.getNivelCargaPorc() << "% de bateria" << std::endl;
+        
+        return true;
+
+    }
+
+    void descargar(int porcentaje){
+
+        int nuevoNivel = bateria.getNivelCargaPorc() - porcentaje;
+        bateria.setNivelCargaPorc(nuevoNivel);
+    }
 };
 
 int main() {
